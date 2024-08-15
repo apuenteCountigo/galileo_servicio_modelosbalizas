76a68d851e6ceef41946ce69544cb9918732de7f
apuentes+1-at-178934116267 <apuente@soaint.com>
2024-08-15 10:50:46 -0400

    INICIALIZANDO PROYECTO

--- servicio-modelosbalizas/src/main/java/com/galileo/cu/servicio_modelosbalizas/ServicioModelosbalizasApplication.java
+++ servicio-modelosbalizas/src/main/java/com/galileo/cu/servicio_modelosbalizas/ServicioModelosbalizasApplication.java
@@ -2,12 +2,25 @@ package com.galileo.cu.servicio_modelosbalizas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-public class ServicioModelosbalizasApplication {
+@EnableEurekaClient
+@EnableFeignClients
+@EntityScan({ "com.galileo.cu.commons.models" })
+public class ServicioModelosbalizasApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServicioModelosbalizasApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("**************************************");
+		System.out.println("Importador V-2024-08-15 10:48");
 	}
 
 }
