9f2e0b9cab66e3185fc1eb429b0415cd17eb0fd0
apuentes+1-at-178934116267 <apuente@soaint.com>
2024-08-15 10:36:05 -0400

    INICIO

---
+++ servicio-modelosbalizas/src/main/java/com/galileo/cu/servicio_modelosbalizas/ServicioModelosbalizasApplication.java
@@ -0,0 +1,13 @@
+package com.galileo.cu.servicio_modelosbalizas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioModelosbalizasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicioModelosbalizasApplication.class, args);
+	}
+
+}
