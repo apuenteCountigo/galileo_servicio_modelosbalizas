54fab4c8554ca4bc3380ce440713a6200e429553
apuentes+1-at-178934116267 <apuente@soaint.com>
2024-08-15 11:48:33 -0400

    creando ModelosBalizasRepository

---
+++ servicio-modelosbalizas/src/main/java/com/galileo/cu/servicio_modelosbalizas/repositorio/ModelosBalizasRepository.java
@@ -0,0 +1,12 @@
+package com.galileo.cu.servicio_modelosbalizas.repositorio;
+
+import com.galileo.cu.commons.models.ModelosBalizas;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin
+@RepositoryRestResource(collectionResourceRel = "modelosbalizas", path = "modelosbalizas")
+public interface ModelosBalizasRepository extends PagingAndSortingRepository<ModelosBalizas, Long> {
+    ModelosBalizas findFirstByDescripcion(String descripcion);
+}
